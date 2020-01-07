c179d6c5ecdfc7c9af84ddec1ac0ba138b7778dc
minhajspace <minhaj.space@gmail.com>
2020-01-07 21:29:19 +0530

    changes in design

--- src/App.js
+++ src/App.js
@@ -2,19 +2,23 @@ import React from "react";
 import Todoinput from './InputTodo'
 
 
-let today = new Date().toLocaleDateString()
-
+let today = new Date().toISOString().slice(0, 10)
 
 class App extends React.Component {
 
 
   render() {
-    return <div className="ui segment">
-      <center><h1>Todo List</h1></center>
+
+    return (<>
+      <div className="ui segment">
+        <center><h1>Todo List</h1></center>
+      </div>
       <div className="ui segment">
         <center><h3>{today}</h3></center>
       </div>
-    </div>;
+    </>)
+
+
   }
 }
 export default App;
