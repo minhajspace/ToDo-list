210d75152e4cb4742da6efa2ad97b2175ebb8126
minhajspace <minhaj.space@gmail.com>
2020-01-07 21:52:33 +0530

    adding text field

--- src/InputTodo.js
+++ src/InputTodo.js
@@ -1,9 +1,31 @@
 import React from 'react'
 
 class Todoinput extends React.Component {
+    state = {
+        userInput: []
+    }
+
+
     render() {
         return (
-            <div></div>
+            <div className="ui segment">
+
+
+                <div className="ui fluid icon input transparent ">
+
+                    <input className=""
+                        type="text"
+                        placeholder="Enter your Todo task"
+                        value={this.state.userInput}
+
+                    />
+                </div>
+
+
+
+
+
+            </div>
         )
     }
 }
--- src/index.js
+++ src/index.js
@@ -1,8 +1,19 @@
 import React from "react";
 import ReactDOM from "react-dom";
 import App from "./App";
+import Todoinput from "./InputTodo";
 
-ReactDOM.render(<App />, document.getElementById("root"));
+const Index = () => {
+    return (
+        <>
+            <App />
+            <Todoinput />
+        </>
+    )
+
+}
+
+ReactDOM.render(<Index />, document.getElementById("root"));
 
 // If you want your app to work offline and load faster, you can change
 // unregister() to register() below. Note this comes with some pitfalls.
