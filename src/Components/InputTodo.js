176dddaf6b77210bd598fec5a28f375569f3a6c7
minhajspace <minhaj.space@gmail.com>
2020-11-16 13:39:59 +0530

    change in file location

--- src/Components/InputTodo.js
+++ src/Components/InputTodo.js
@@ -1,73 +1,121 @@
-import React from 'react'
-
-
-let arr = [];
-let tododata;
-
-
-
-
-
-class Todoinput extends React.Component {
+import React from "react";
+import { Checkbox } from "semantic-ui-react";
+import EditModal from "./Editmodal";
+import "./styles.css";
+import CleanModal from "./CleanModal";
+class InputTodo extends React.Component {
     state = {
-        isTrue: true,
-        userInput: "",
-        todolist: [],
-    }
- 
-    onUserInput = (e) => {
-        this.setState({ userInput: e.target.value })
-    }
-    onButtonClick = (e) => {
-        e.preventDefault();
-        tododata = this.state.userInput
-        arr.push(tododata)
-        this.setState({ todolist: arr })
-        this.setState({ userInput: "" })
+        list: [],
+        text: ""
+    };
 
-    }
-    onDoneClick = (e) => {
+    onInputChange = (e) => {
         e.preventDefault();
-    }
-    onEditClick = (e) => {
-        e.preventDefault();
-    }
+        this.setState({
+            text: e.target.value
+        });
+    };
 
+    handleSave = (e) => {
+        if (this.state.text.trim() !== "") {
+            const item = {
+                key: new Date(),
+                text: this.state.text.trim(),
+                isCompleted: false,
+                edit: false
+            };
+            this.setState({
+                list: [...this.state.list, item],
+                text: ""
+            });
+        }
+    };
+
+    handleDelete = (key) => {
+        const filterList = this.state.list.filter((items) => items.key !== key);
+        this.setState({ list: filterList });
+    };
+
+    handleEdit = (input, key) => {
+        const newTodo = this.state.list.map((value) => {
+            if (key === value.key) {
+                value.text = input.text;
+            }
+            return value;
+        });
+        this.setState({ list: newTodo });
+        return newTodo;
+    };
+    handleClearList = () => {
+        this.setState({ list: [] });
+    };
+    handleComplete = (key) => {
+        const newTodo = this.state.list.map((item) => {
+            if (item.key === key) {
+                item.isCompleted = !item.isCompleted;
+            }
+            return item;
+        });
+        this.setState({ list: newTodo });
+        return newTodo;
+    };
+    handleModal = () => { };
     render() {
-
         return (
-            <>
-                <form>
-                    <div className="ui segment">
-
-                        <div className="ui fluid icon input transparent ">
-
-                            <input className=""
-                                type="text"
-                                placeholder="Enter your Todo task"
-                                value={this.state.userInput}
-                                onChange={this.onUserInput}
-                            />
-                        </div>
+            <div className="ui segment">
+                <div style={{ marginBottom: "2%" }} className="ui fluid icon input ">
+                    <input
+                        type="text"
+                        placeholder="Enter your Task"
+                        onChange={this.onInputChange}
+                        value={this.state.text}
+                    />
+                </div>
+                <div className="ui buttons fluid">
+                    <button className="ui blue basic button " onClick={this.handleSave}>
+                        <i className="save icon"></i>
+                    </button>
+                </div>
+                {
+                    <div className={this.state.list.length === 0 ? "" : "ui segment "}>
+                        {this.state.list.map((value) => (
+                            <div
+                                key={value.key}
+                                className={
+                                    value.isCompleted ? "ui segment green" : "ui segment"
+                                }
+                            >
+                                <div>
+                                    {value.text}
+                                    <span className=" right ">
+                                        <Checkbox
+                                            className="checkbox"
+                                            onChange={() => this.handleComplete(value.key)}
+                                            checked={value.isCompleted}
+                                        />
+                                    </span>
+                                    <span
+                                        className="right icon-size"
+                                        onClick={() => this.handleDelete(value.key)}
+                                    >
+                                        <i className="trash icon"></i>
+                                    </span>
+                                    <EditModal
+                                        key={value.key}
+                                        handleEdit={this.handleEdit}
+                                        data={value}
+                                    />
+                                </div>
+                            </div>
+                        ))}
                     </div>
-                    <div className="ui buttons fluid">
-
-                        <button className="ui positive button " onClick={this.onButtonClick}>Save</button>
-                    </div>
-
-                    {this.state.todolist.map((items, key) => (<div className="ui segment">{items}
-
-                        <button className="ui right floated  red basic button " onClick={this.onEditClick}>Edit</button>
-                        <button className="ui right floated  red basic button" onClick={this.onDoneClick}>Done</button>
-
-                    </div>))}
-
-                </form>
-            </>
-
-        )
+                }
+                {this.state.list.length >= 2 && (
+                    <CleanModal handleClearList={this.handleClearList} />
+                )}
+            </div>
+        );
     }
 }
 
-
-export default Todoinput;
+export default InputTodo;
